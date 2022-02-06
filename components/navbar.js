34e8fac5eacb5310c4150883c115f41a57d99ad5
mosesakor <gabrielmosesakor@gmail.com>
2022-02-06 10:56:42 +0100

    updated link to portoflio source code

--- components/navbar.js
+++ components/navbar.js
@@ -79,7 +79,7 @@ const Navbar = props => {
           </LinkItem>
           <LinkItem
             _target="_blank"
-            href="https://github.com/mosesakor/mosesakor-portfolio"
+            href="https://github.com/mosesakor/my-portfolio"
             path={path}
             display="inline-flex"
             alignItems="center"
